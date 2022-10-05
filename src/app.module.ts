6f01c3c5fb1c38e04d0596717dcc2e68bb6fae24
gitkolihis <adelaniolanrewajuisrael@gmail.com>
2022-10-05 13:01:45 +0100

    Database with Postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { PostsController } from './posts/posts.controller';
 import { HashtagsController } from './hashtags/hashtags.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/users.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'twitteradmin',
+      password: 'twitterpass',
+      database: 'twitterdb',
+      entities: [UserEntity],
+      synchronize: true,
+      logging: 'all',
+      logger: 'advanced-console',
+    }),
+  ],
   controllers: [
     AppController,
     UsersController,
