66875d2678dd92345fbd25afc2c3a5460a1bff06
gitkolihis <adelaniolanrewajuisrael@gmail.com>
2022-10-05 14:29:17 +0100

    Add Post Entity

---
+++ src/posts/post.entity.ts
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+import { TwitterBaseEntity } from 'src/common/base.entity';
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+
+@Entity('posts')
+export class PostEntity extends TwitterBaseEntity {
+
+    @Column({ length: 240, nullable: true})
+    text: string;
+
+    @Column({ length: 50 })
+    author_id: string
+
+    @Column('json', { default   : []})
+    images: Array<string>;
+
+    @Column({ name: 'like_count', default: 0 })
+    likeCount: number;
+
+    @Column({ name: 'repost_count', default: 0})
+    repostCount: number;
+
+    @Column('json', { default: []})
+    hashtags: Array<string>;
+
+    @Column('json', {default: []})
+    mentions: Array<Mention>
+
+    @OneToOne(() => PostEntity)
+    @JoinColumn({name : 'orig_post_id'})
+    origPost: PostEntity
+
+    @OneToOne(() => PostEntity)
+    @JoinColumn({ name: 'reply_to_id'})
+    replyPost: PostEntity
+}
+
+
+class Mention {
+    name: string
+    id: string
+}
